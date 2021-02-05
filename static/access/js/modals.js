f59718fa930232f2447561e7a885f74673186c24
CosmoBot <vitaliy.brooks@gmail.com>
2021-02-06 03:05:14 +0700

    New template LG

---
+++ static/access/js/modals.js
@@ -0,0 +1,50 @@
+function modal_window_open(content = null) {
+    document.getElementsByClassName("global_block")[0].style.display = "block";
+    if (content == "query_tel") {
+        return document.getElementsByClassName("win1")[0].style.display = "block";
+    }
+}
+
+function modal_window_close(content_next = null) {
+    document.getElementsByClassName("global_block")[0].style.display = "none";
+    if (content_next == "query_tel") {
+        return document.getElementsByClassName("win1")[0].style.display = "none";
+    }
+}
+
+
+query_tel[0].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[1].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[2].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[3].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[4].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[5].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[6].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[7].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[8].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+query_tel[9].onclick = function() {
+    modal_window_open(content = "query_tel");
+}
+
+
+query_tel_close.onclick = function() {
+    modal_window_close(content_next = "query_tel");
+}
